01ff8dcb7a8bdb63d89208b14f190607bdf68551
yuhya <akiyama@zaty.jp>
2022-08-06 14:35:15 +0900

    クロスオリジンを許可

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
 $year = $_GET['year'];
 $month = $_GET['month'];
 
